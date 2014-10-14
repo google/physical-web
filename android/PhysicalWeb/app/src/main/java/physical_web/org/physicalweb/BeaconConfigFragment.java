c0ea0014f40893ce628a912b6e420746ba1280df
Giovanni Ortuño <gd.ortuno@gmail.com>
2014-10-13 21:01:55 -0700

    No need no initialize the view before returning

--- android/PhysicalWeb/app/src/main/java/physical_web/org/physicalweb/BeaconConfigFragment.java
+++ android/PhysicalWeb/app/src/main/java/physical_web/org/physicalweb/BeaconConfigFragment.java
@@ -141,8 +141,7 @@ public class BeaconConfigFragment extends Fragment implements BeaconConfigHelper
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    View view = inflater.inflate(R.layout.fragment_beacon_config, container, false);
-    return view;
+    return inflater.inflate(R.layout.fragment_beacon_config, container, false);
   }
 
   @Override
