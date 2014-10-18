6ac079c07fc9ab71647dcfcc888a1a2ee518950c
Matthew Sibigtroth <matthew.sibigtroth@gmail.com>
2014-10-18 09:32:51 -0700

    Version.properties fix

--- android/PhysicalWeb/app/src/main/java/physical_web/org/physicalweb/MainActivity.java
+++ android/PhysicalWeb/app/src/main/java/physical_web/org/physicalweb/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
     if (savedInstanceState == null) {
       showNearbyBeaconsFragment(false);
     }
