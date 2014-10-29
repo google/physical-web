15d21f9d945db327b5e87332b30a82a3f7cc6f18
Amit Agrawal <asagrawal@gmail.com>
2014-10-28 22:59:56 -0700

    Update build.sh with pull request changes
     - gradlew is already an executable
    - make script POSIX compatible by using /bin/sh

--- travis/build.sh
+++ travis/build.sh
@@ -1,10 +1,7 @@
-#/usr/bin/env/sh
+#!/bin/sh
 
 #browse to the android project
 cd android/PhysicalWeb/
-
-#grant file execute permissions
-chmod +x ./gradlew
 
 #build a specific gradle task
 ./gradlew assembleRelease
