45864ee91ff6d5183bc5b0dcb1d933af90151b52
Louay Bassbouss <louay.bassbouss@fokus.fraunhofer.de>
2015-02-11 13:42:51 +0100

    * minor bug fix * documentations updated

--- chrome/js/index.js
+++ chrome/js/index.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2014 Fraunhofer FOKUS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * AUTHOR: Louay Bassbouss <louay.bassbouss@fokus.fraunhofer.de>
+ *
+ */
+ var SCAN_TIMEOUT = 4000;
 (function(){
 	var scanning = false;
 	var urls = {};
@@ -36,7 +55,7 @@ document.addEventListener("receiveUrl",function(evt){
 });
 document.addEventListener("requestRefresh",function(evt){
 	console.log("receiveUrl",evt);
-	startScan(4000);
+	startScan(SCAN_TIMEOUT);
 });
 var requestUrlMetaData = function(urls){
 	var objects = [];
@@ -116,7 +135,7 @@ $(function(){
 		chrome.app.window.current().minimize();
 	};
 	document.getElementById("refreshBtn").onclick = function(){
-		startScan(4000);
+		startScan(SCAN_TIMEOUT);
 	};
-	startScan(4000);
+	startScan(SCAN_TIMEOUT);
 });
--- chrome/js/peer-ssdp.js
+++ chrome/js/peer-ssdp.js
@@ -1,23 +1,21 @@
-/*******************************************************************************
- * 
- * Copyright (c) 2013 Louay Bassbouss, Fraunhofer FOKUS, All rights reserved.
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>. 
- * 
- * AUTHORS: Louay Bassbouss (louay.bassbouss@fokus.fraunhofer.de)
+/*
+ * Copyright 2014 Fraunhofer FOKUS
  *
- ******************************************************************************/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * AUTHOR: Louay Bassbouss <louay.bassbouss@fokus.fraunhofer.de>
+ *
+ */
 (function(exports){
 	var SSDP_ADDRESS = "239.255.255.250";
 	var SSDP_PORT = 1900;
