5f6fbc9aa3c0aaf20984e255121641b2dfd2ff98
Jonas Sevcik <sevcik@mobera.eu>
2015-07-23 08:30:40 +0200

    split line loner than 100 chars

--- android/PhysicalWeb/app/src/main/java/org/physical_web/physicalweb/wifi/UrlUtil.java
+++ android/PhysicalWeb/app/src/main/java/org/physical_web/physicalweb/wifi/UrlUtil.java
@@ -10,31 +10,30 @@ import java.util.regex.Pattern;
  */
 public final class UrlUtil {
 
-  private static final String
-      URL_REGEX =
-      "(http|https)://([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?(([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})|([0-9]+\\.){3}[0-9]{1})(:[0-9]+)?(/\\S*)?";
-  private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
+    private static final String URL_REGEX = "(http|https)://([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?"
+            + "(([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})|([0-9]+\\.){3}[0-9]{1})(:[0-9]+)?(/\\S*)?";
+    private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
 
-  private UrlUtil() {
-    //don't instantiate
-  }
-
-  public static boolean containsUrl(String ssid) {
-    return ssid.matches("(.+\\s|\\s)*" + URL_REGEX + "(\\s.+|\\s)*");
-  }
-
-  /**
-   * Extracts URL from a String.
-   *
-   * @param ssid containing URL
-   * @return first found url; if {@code ssid} doesn't contain URL, returns null
-   */
-  @Nullable
-  public static String extractUrl(String ssid) {
-    Matcher matcher = URL_PATTERN.matcher(ssid);
-    if (matcher.find()) {
-      return matcher.group();
+    private UrlUtil() {
+        //don't instantiate
     }
-    return null;
-  }
+
+    public static boolean containsUrl(String ssid) {
+        return ssid.matches("(.+\\s|\\s)*" + URL_REGEX + "(\\s.+|\\s)*");
+    }
+
+    /**
+     * Extracts URL from a String.
+     *
+     * @param ssid containing URL
+     * @return first found url; if {@code ssid} doesn't contain URL, returns null
+     */
+    @Nullable
+    public static String extractUrl(String ssid) {
+        Matcher matcher = URL_PATTERN.matcher(ssid);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return null;
+    }
 }
