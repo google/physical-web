ad06dddd9c0bc4fcb3fa0b0e124c37556806d4f4
Conley Owens <cco3@google.com>
2015-10-06 10:48:37 -0700

    Create a SimplePwo class
    * Create a Pwo interface
    * Create a SimplePwo class that just has an ID and a URL
    * Add methods to add and fetch Pwos from PwoCollection

--- java/libs/src/test/java/org/physical_web/collection/PwoCollectionTest.java
+++ java/libs/src/test/java/org/physical_web/collection/PwoCollectionTest.java
@@ -22,8 +22,24 @@ import static org.junit.Assert.*;
  * PwoCollection unit test class.
  */
 public class PwoCollectionTest {
+  private final String ID1 = "id1";
+  private final String ID2 = "id2";
+  private final String URL1 = "http://example.com";
+
   @Test
-  public void testPwoCollection() {
+  public void getPwoByIdReturnsFoundPwo() {
     PwoCollection pwoCollection = new PwoCollection();
+    Pwo addedPwo = new SimplePwo(ID1, URL1);
+    pwoCollection.addPwo(addedPwo);
+    Pwo fetchedPwo = pwoCollection.getPwoById(ID1);
+    assertEquals(fetchedPwo.getId(), ID1);
+    assertEquals(fetchedPwo.getUrl(), URL1);
+  }
+
+  @Test
+  public void getPwoByReturnsNullForMissingPwo() {
+    PwoCollection pwoCollection = new PwoCollection();
+    Pwo fetchedPwo = pwoCollection.getPwoById(ID1);
+    assertNull(fetchedPwo);
   }
 }
