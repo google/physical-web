8db88fe143647671f0f32313cb3d6688de11d194
Jordan Hayes <hayesjordan@google.com>
2016-08-23 10:43:16 -0700

    Improve FatBeacon implementation
    Make FatBeacon download cancelable

--- android/PhysicalWeb/app/src/main/java/org/physical_web/physicalweb/BluetoothSite.java
+++ android/PhysicalWeb/app/src/main/java/org/physical_web/physicalweb/BluetoothSite.java
@@ -22,8 +22,9 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build.VERSION;
@@ -31,11 +32,12 @@ import android.os.Build.VERSION_CODES;
 import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.widget.Toast;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
+import java.util.UUID;
 
 /**
  * This class is used to connect to a ble gatt service and download a web page.
@@ -43,8 +45,11 @@ import java.util.List;
 public class BluetoothSite extends BluetoothGattCallback {
 
   private static final String TAG = BluetoothSite.class.getSimpleName();
-  private static final String SERVICE_UUID = "ae5946d4-e587-4ba8-b6a5-a97cca6affd3";
+  private static final UUID SERVICE_UUID = UUID.fromString("ae5946d4-e587-4ba8-b6a5-a97cca6affd3");
+  private static final UUID CHARACTERISTIC_WEBPAGE_UUID = UUID.fromString(
+      "d1a517f0-2499-46ca-9ccc-809bc1c966fa");
   private Activity activity;
+  private BluetoothGatt mBluetoothGatt;
   private BluetoothGattCharacteristic characteristic;
   private ProgressDialog progress;
   private int transferRate = 20;
@@ -62,8 +67,18 @@ public class BluetoothSite extends BluetoothGattCallback {
    */
   public void connect(String deviceAddress, String title) {
     String progressTitle = activity.getString(R.string.page_loading_title) + " " + title;
-    progress = ProgressDialog.show(activity, progressTitle,
-        activity.getString(R.string.page_loading_message), true);
+    progress = new ProgressDialog(activity);
+    progress.setCancelable(true);
+    progress.setOnCancelListener(new OnCancelListener() {
+      @Override
+      public void onCancel(DialogInterface dialogInterface) {
+        Log.i(TAG, "Dialog box canceled");
+        close();
+      }
+    });
+    progress.setTitle(progressTitle);
+    progress.setMessage(activity.getString(R.string.page_loading_message));
+    progress.show();
     BluetoothAdapter.getDefaultAdapter().getRemoteDevice(deviceAddress)
         .connectGatt(activity, false, this);
   }
@@ -73,6 +88,7 @@ public class BluetoothSite extends BluetoothGattCallback {
       int status) {
     if (status == BluetoothGatt.GATT_SUCCESS
         && characteristic.getValue().length < transferRate) {
+      Log.i(TAG, "onCharacteristicRead successful: small packet");
       // Transfer is complete
       html.append(new String(characteristic.getValue()));
       progress.dismiss();
@@ -85,12 +101,13 @@ public class BluetoothSite extends BluetoothGattCallback {
         openInChrome(file);
       }
     } else if (status == BluetoothGatt.GATT_SUCCESS) {
+      Log.i(TAG, "onCharacteristicRead successful: full packet");
       // Full packet received, check for more data
       html.append(new String(characteristic.getValue()));
       gatt.readCharacteristic(this.characteristic);
     } else {
       Log.i(TAG, "onCharacteristicRead unsuccessful: " + status);
-      gatt.disconnect();
+      close();
       progress.dismiss();
       Toast.makeText(activity, R.string.ble_download_error_message, Toast.LENGTH_SHORT).show();
     }
@@ -100,6 +117,7 @@ public class BluetoothSite extends BluetoothGattCallback {
   public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
     if (newState == BluetoothProfile.STATE_CONNECTED) {
       Log.i(TAG, "Connected to GATT server");
+      mBluetoothGatt = gatt;
       html = new StringBuilder("");
       if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
         gatt.requestConnectionPriority(BluetoothGatt.CONNECTION_PRIORITY_HIGH);
@@ -124,14 +142,17 @@ public class BluetoothSite extends BluetoothGattCallback {
   public void onServicesDiscovered(BluetoothGatt gatt, int status) {
     Log.i(TAG, "Services Discovered");
     if (status == BluetoothGatt.GATT_SUCCESS) {
-      List<BluetoothGattService> services = gatt.getServices();
-      for (BluetoothGattService gattService : services) {
-        if (gattService.getUuid().toString().equals(SERVICE_UUID)) {
-          characteristic = gattService.getCharacteristics().get(0);
-          gatt.readCharacteristic(characteristic);
-        }
-      }
+      characteristic = gatt.getService(SERVICE_UUID).getCharacteristic(CHARACTERISTIC_WEBPAGE_UUID);
+      gatt.readCharacteristic(characteristic);
     }
+  }
+
+  private void close() {
+    if (mBluetoothGatt == null) {
+      return;
+    }
+    mBluetoothGatt.close();
+    mBluetoothGatt = null;
   }
 
   private void openInChrome(File file) {
